30856695ce09d9f28deb2b3cc0ab9e8fb7850d9b
ymurai <y.murai.pc@gmail.com>
2025-09-15 14:56:45 +0900

    Creating a Sprite

--- js/hanson.js
+++ js/hanson.js
@@ -13,4 +13,20 @@
 
   // Then adding the application's canvas to the DOM body.
   document.body.querySelector('.game-screen').appendChild(app.canvas);
+
+  // Load the bunny texture.
+  const texture = await PIXI.Assets.load('https://pixijs.com/assets/bunny.png');
+
+  // Create a new Sprite from an image path
+  const bunny = new PIXI.Sprite(texture);
+
+  // Add to stage
+  app.stage.addChild(bunny);
+
+  // Center the sprite's anchor point
+  bunny.anchor.set(0.5);
+
+  // Move the sprite to the center of the screen
+  bunny.x = app.screen.width / 2;
+  bunny.y = app.screen.height / 2;
 })();
