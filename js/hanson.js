911d9b421e530e87677734ac42d578ed378b6205
ymurai <y.murai.pc@gmail.com>
2025-09-19 15:27:45 +0900

    Adding Stars

--- js/hanson.js
+++ js/hanson.js
@@ -1,22 +1,19 @@
 // import { Application, Assets, Container, Sprite } from 'pixi.js';
+import { addStars } from './addStars.js';
 
 (async () => {
   // Create a new application
   const app = new PIXI.Application();
 
-  // Store an array of fish sprites for animation.
-  const fishes = [];
+  // Intialize the application.
+  await app.init({
+    background: '#021f4b',
+    width: 800,
+    height: 600,
+  });
 
-  async function setup() {
-    // Intialize the application.
-    await app.init({
-      background: '#021f4b',
-      width: 800,
-      height: 600,
-    });
+  // Then adding the application's canvas to the DOM body.
+  document.body.querySelector('.game-screen').appendChild(app.canvas);
 
-    // Then adding the application's canvas to the DOM body.
-    document.body.querySelector('.game-screen').appendChild(app.canvas);
-  }
-
+  addStars(app);
 })();
