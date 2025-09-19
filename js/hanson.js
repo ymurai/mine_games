b43f205163d9cbeac963baee354015990c01d7f7
ymurai <y.murai.pc@gmail.com>
2025-09-19 15:22:46 +0900

    Onboard the Choo Choo Train!

--- js/hanson.js
+++ js/hanson.js
@@ -1,9 +1,4 @@
 // import { Application, Assets, Container, Sprite } from 'pixi.js';
-import { addBackground } from './addBackground.js';
-import { addDisplacementEffect } from './addDisplacementEffect.js';
-import { addFishes, animateFishes } from './addFishes.js';
-import { addWaterOverlay, animateWaterOverlay } from './addWaterOverlay.js';
-
 
 (async () => {
   // Create a new application
@@ -15,7 +10,7 @@ import { addWaterOverlay, animateWaterOverlay } from './addWaterOverlay.js';
   async function setup() {
     // Intialize the application.
     await app.init({
-      background: '#1099bb',
+      background: '#021f4b',
       width: 800,
       height: 600,
     });
@@ -23,39 +18,5 @@ import { addWaterOverlay, animateWaterOverlay } from './addWaterOverlay.js';
     // Then adding the application's canvas to the DOM body.
     document.body.querySelector('.game-screen').appendChild(app.canvas);
   }
-
-  async function preload() {
-    // Create an array of asset data to load.
-    const assets = [
-      { alias: 'background', src: 'https://pixijs.com/assets/tutorials/fish-pond/pond_background.jpg' },
-      { alias: 'fish1', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish1.png' },
-      { alias: 'fish2', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish2.png' },
-      { alias: 'fish3', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish3.png' },
-      { alias: 'fish4', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish4.png' },
-      { alias: 'fish5', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish5.png' },
-      { alias: 'overlay', src: 'https://pixijs.com/assets/tutorials/fish-pond/wave_overlay.png' },
-      { alias: 'displacement', src: 'https://pixijs.com/assets/tutorials/fish-pond/displacement_map.png' },
-    ];
-
-    // Load the assets defined above.
-    await PIXI.Assets.load(assets);
-  }
-
-  // Asynchronous IIFE
-  (async () => {
-    await setup();
-    await preload();
-
-    addBackground(app);
-    addFishes(app, fishes);
-    addWaterOverlay(app);
-    addDisplacementEffect(app);
-
-    // Add the fish animation callback to the application's ticker.
-    app.ticker.add((time) => {
-      animateFishes(app, fishes, time);
-      animateWaterOverlay(app, time);
-    })
-  })();
 
 })();
